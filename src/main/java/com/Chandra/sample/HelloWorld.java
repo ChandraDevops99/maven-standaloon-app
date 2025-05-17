e27b07808f4a56822f36bf955e4d1ad50bd07753
CHANDRAMOULI UYYALA <uyyala99@gmail.com>
2025-05-17 16:52:58 +0530

    Update HelloWorld.java

--- src/main/java/com/Chandra/sample/HelloWorld.java
+++ src/main/java/com/Chandra/sample/HelloWorld.java
@@ -1,21 +1,12 @@
 package com.Chandra.sample;
 
-/**
- * Hello world!
- *
- */
-import java.util.Date;
-public class HelloWorld {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
-
-	}
-
-	private static Date getLocalCurrentDate() {
-		String m = "KK DEVOPS";
-		return new Date();
-
-	}
-
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() {
+        HelloWorld hw = new HelloWorld();
+        assertEquals("Hello, World!", hw.sayHello());
+    }
 }
