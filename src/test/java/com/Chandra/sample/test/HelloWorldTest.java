7791584d330f7d3bc30a135daba033ebbe8ece24
CHANDRAMOULI UYYALA <uyyala99@gmail.com>
2025-05-17 17:17:18 +0530

    Update HelloWorldTest.java

--- src/test/java/com/Chandra/sample/test/HelloWorldTest.java
+++ src/test/java/com/Chandra/sample/test/HelloWorldTest.java
@@ -1,1 +1,13 @@
+package com.Chandra.sample;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSayHello() {
+        HelloWorld hw = new HelloWorld();
+        assertEquals("Hello, World!", hw.sayHello());
+    }
+}
